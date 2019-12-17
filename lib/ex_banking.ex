5dfeb4350e30b9d66f0156c9dab6b8562ad129d5
Desire <dafewou@gmail.com>
2019-12-18 00:53:12 +0800

    Add type checking on params

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -21,80 +21,96 @@ defmodule ExBanking do
 
   @spec create_user(user :: String.t()) :: :ok | banking_error
   def create_user(user) do
-    case Registry.get(:banking_registry, user) do
-      {:ok, _} ->
-        {:error, :user_already_exists}
+    if !is_binary(user) do
+      {:error, :wrong_arguments}
+    else
+      case Registry.get(:banking_registry, user) do
+        {:ok, _} ->
+          {:error, :user_already_exists}
 
-      {:error, :not_found} ->
-        user_account = %{
-          "name" => user,
-          "myr" => 0
-        }
+        {:error, :not_found} ->
+          user_account = %{
+            "name" => user,
+            "myr" => 0
+          }
 
-        Registry.create(:banking_registry, {user, user_account})
+          Registry.create(:banking_registry, {user, user_account})
+      end
     end
   end
 
   @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
           {:ok, new_balance :: number} | banking_error
   def deposit(user, amount, currency) do
-    case Registry.get(:banking_registry, user) do
-      {:ok, user_account} ->
-        new_balance =
-          if is_nil(user_account[currency]) do
-            amount
-          else
-            user_account[currency] + amount
-          end
+    if !is_binary(user) || !is_number(amount) || !is_binary(currency) do
+      {:error, :wrong_arguments}
+    else
+      case Registry.get(:banking_registry, user) do
+        {:ok, user_account} ->
+          new_balance =
+            if is_nil(user_account[currency]) do
+              amount
+            else
+              user_account[currency] + amount
+            end
 
-        user_account =
-          user_account
-          |> Map.put(currency, new_balance)
+          user_account =
+            user_account
+            |> Map.put(currency, new_balance)
 
-        Registry.create(:banking_registry, {user, user_account})
+          Registry.create(:banking_registry, {user, user_account})
 
-        {:ok, new_balance}
+          {:ok, new_balance}
 
-      {:error, :not_found} ->
-        {:error, :user_does_not_exist}
+        {:error, :not_found} ->
+          {:error, :user_does_not_exist}
+      end
     end
   end
 
   @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
           {:ok, new_balance :: number} | banking_error
   def withdraw(user, amount, currency) do
-    case Registry.get(:banking_registry, user) do
-      {:ok, user_account} ->
-        if !is_nil(user_account[currency]) && user_account[currency] >= amount do
-          user_account =
-            user_account
-            |> Map.put(currency, user_account[currency] - amount)
+    if !is_binary(user) || !is_number(amount) || !is_binary(currency) do
+      {:error, :wrong_arguments}
+    else
+      case Registry.get(:banking_registry, user) do
+        {:ok, user_account} ->
+          if !is_nil(user_account[currency]) && user_account[currency] >= amount do
+            user_account =
+              user_account
+              |> Map.put(currency, user_account[currency] - amount)
 
-          Registry.create(:banking_registry, {user, user_account})
+            Registry.create(:banking_registry, {user, user_account})
 
-          {:ok, user_account[currency]}
-        else
-          {:error, :not_enough_money}
-        end
+            {:ok, user_account[currency]}
+          else
+            {:error, :not_enough_money}
+          end
 
-      {:error, :not_found} ->
-        {:error, :user_does_not_exist}
+        {:error, :not_found} ->
+          {:error, :user_does_not_exist}
+      end
     end
   end
 
   @spec get_balance(user :: String.t(), currency :: String.t()) ::
           {:ok, balance :: number} | banking_error
   def get_balance(user, currency) do
-    case Registry.get(:banking_registry, user) do
-      {:ok, user_account} ->
-        if !is_nil(user_account[currency]) do
-          {:ok, user_account[currency]}
-        else
-          {:error, :not_enough_money}
-        end
+    if !is_binary(user) || !is_binary(currency) do
+      {:error, :wrong_arguments}
+    else
+      case Registry.get(:banking_registry, user) do
+        {:ok, user_account} ->
+          if !is_nil(user_account[currency]) do
+            {:ok, user_account[currency]}
+          else
+            {:error, :not_enough_money}
+          end
 
-      {:error, :not_found} ->
-        {:error, :user_does_not_exist}
+        {:error, :not_found} ->
+          {:error, :user_does_not_exist}
+      end
     end
   end
 
@@ -105,23 +121,27 @@ defmodule ExBanking do
           currency :: String.t()
         ) :: {:ok, from_user_balance :: number, to_user_balance :: number} | banking_error
   def send(from_user, to_user, amount, currency) do
-    case withdraw(from_user, amount, currency) do
-      {:ok, from_user_balance} ->
-        case deposit(to_user, amount, currency) do
-          {:ok, to_user_balance} ->
-            {:ok, from_user_balance, to_user_balance}
+    if !is_binary(from_user) || !is_binary(to_user) || !is_number(amount) || !is_binary(currency) do
+      {:error, :wrong_arguments}
+    else
+      case withdraw(from_user, amount, currency) do
+        {:ok, from_user_balance} ->
+          case deposit(to_user, amount, currency) do
+            {:ok, to_user_balance} ->
+              {:ok, from_user_balance, to_user_balance}
 
-          {:error, :user_does_not_exist} ->
-            # Reverse transaction in case receiver does not exist
-            deposit(from_user, amount, currency)
-            {:error, :receiver_does_not_exist}
-        end
+            {:error, :user_does_not_exist} ->
+              # Reverse transaction in case receiver does not exist
+              deposit(from_user, amount, currency)
+              {:error, :receiver_does_not_exist}
+          end
 
-      {:error, :not_enough_money} ->
-        {:error, :not_enough_money}
+        {:error, :not_enough_money} ->
+          {:error, :not_enough_money}
 
-      {:error, :user_does_not_exist} ->
-        {:error, :sender_does_not_exist}
+        {:error, :user_does_not_exist} ->
+          {:error, :sender_does_not_exist}
+      end
     end
   end
 end
